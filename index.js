4645f6753b9484446a0316b04af5c76a507f9dc2
chollingsworth <caleb.hollingsworth10@gmail.com>
2020-08-05 12:17:49 -0500

    added colors

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 #! /usr/bin/env node
 
 const fs = require('fs');
+var colors = require('colors');
 
 fs.readFile(__dirname + '/info.json', 'utf8', function (err, data) {
 	if (err) {
 		console.error(err);
 	} else {
-		console.log(data);
+		console.log(data.america);
 		return data;
 	}
 });
